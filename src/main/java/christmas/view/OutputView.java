56d61010f09d9c0baf51925c8b7c7ebb5bc6c744
irerin07 <counterchord89@gmail.com>
2023-11-15 23:31:38 +0900

    refactor: #7
    OutputView 리팩터링
    - PrintEventStatistics 메서드가 너무 거대하여 해당 메서드를 주문관련 출력 메서드와 혜택관련 출력 메서드로 나눔
    
    Closes #7

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -13,13 +13,8 @@ public class OutputView {
     public void printEventStatistics(Order order, SaleProfit saleProfit) {
         StringBuilder stringBuilder = new StringBuilder();
 
-        appendSection(stringBuilder, "주문 메뉴", order.printOrderedMenus());
-        appendSection(stringBuilder, "할인 전 총주문 금액", order.printTotalOrderedPrice());
-        appendSection(stringBuilder, "증정 메뉴", saleProfit.getGiftMenu());
-        appendSection(stringBuilder, "혜택 내역", saleProfit.appendBenefits(order));
-        appendSection(stringBuilder, "총혜택 금액", saleProfit.printTotalBenefitAmount());
-        appendSection(stringBuilder, "할인 후 예상 결제 금액", saleProfit.printEstimatedCheckoutPrice(order));
-        appendSection(stringBuilder, "12월 이벤트 배지", saleProfit.getEventBadge());
+        printOrderRelatedInformation(stringBuilder, order);
+        printBenefitsRelatedInformation(stringBuilder, order, saleProfit);
 
         System.out.println(stringBuilder);
     }
@@ -31,6 +26,19 @@ public class OutputView {
 
     private String printTitle(String title) {
         return "<" + title + ">";
+    }
+
+    private void printOrderRelatedInformation(StringBuilder stringBuilder, Order order) {
+        appendSection(stringBuilder, "주문 메뉴", order.printOrderedMenus());
+        appendSection(stringBuilder, "할인 전 총주문 금액", order.printTotalOrderedPrice());
+    }
+
+    private void printBenefitsRelatedInformation(StringBuilder stringBuilder, Order order, SaleProfit saleProfit) {
+        appendSection(stringBuilder, "증정 메뉴", saleProfit.getGiftMenu());
+        appendSection(stringBuilder, "혜택 내역", saleProfit.appendBenefits(order));
+        appendSection(stringBuilder, "총혜택 금액", saleProfit.printTotalBenefitAmount());
+        appendSection(stringBuilder, "할인 후 예상 결제 금액", saleProfit.printEstimatedCheckoutPrice(order));
+        appendSection(stringBuilder, "12월 이벤트 배지", saleProfit.getEventBadge());
     }
 
 }
